8f62ff0f04a1389f6d6c69a4edd91d24dd993eb4
Benoit Moussaud <bmoussaud@xebialabs.com>
2016-06-27 13:04:43 +0200

    formating

--- src/main/resources/docker-machine/ci/create-environment.py
+++ src/main/resources/docker-machine/ci/create-environment.py
@@ -6,23 +6,21 @@
 
 
 from com.xebialabs.deployit.plugin.api.reflect import Type
-from sets import Set
-import sys
 
-def createOrUpdate(ci):
+
+def create_or_update(ci):
     if repositoryService.exists(ci.id):
         print "-- %s : updated" % ci
-        return repositoryService.update(ci.id,ci)
+        return repositoryService.update(ci.id, ci)
     else:
         print "-- %s : created" % ci
-        return repositoryService.create(ci.id,ci)
+        return repositoryService.create(ci.id, ci)
 
-def newInstance(ci_type, ci_id):
+
+def new_instance(ci_type, ci_id):
     return metadataService.findDescriptor(Type.valueOf(ci_type)).newInstance(ci_id)
 
 
 if not repositoryService.exists(ci_id):
-    ci = newInstance(ci_type,ci_id)
-    createOrUpdate(ci)
-
-
+    ci = new_instance(ci_type, ci_id)
+    create_or_update(ci)
--- src/main/resources/docker-machine/ci/create.py
+++ src/main/resources/docker-machine/ci/create.py
@@ -6,35 +6,32 @@
 
 
 from com.xebialabs.deployit.plugin.api.reflect import Type
-from sets import Set
-import sys
 
-def createOrUpdate(ci):
+
+def create_or_update(ci):
     if repositoryService.exists(ci.id):
         print "-- %s : updated" % ci
-        return repositoryService.update(ci.id,ci)
+        return repositoryService.update(ci.id, ci)
     else:
         print "-- %s : created" % ci
-        return repositoryService.create(ci.id,ci)
+        return repositoryService.create(ci.id, ci)
 
-def newInstance(ci_type, ci_id):
+
+def new_instance(ci_type, ci_id):
     return metadataService.findDescriptor(Type.valueOf(ci_type)).newInstance(ci_id)
 
 
 data = context.getAttribute(context_key)
 print data
 
-ci = newInstance(ci_type,ci_id)
+ci = new_instance(ci_type, ci_id)
 ci.address = data['address']
 ci.tls_verify = True if "1" in data['DOCKER_TLS_VERIFY'] else False
-#ci.provider = 'virtualbox'
-#ci.dynamicParameters = True
+# ci.dynamicParameters = True
 ci.certificatePath = data['DOCKER_CERT_PATH']
 print ci_properties
 for key, value in ci_properties.iteritems():
-    print "set ci properties %s=%s" % (key,value)
-    ci.setProperty(key,value)
+    print "set ci properties %s=%s" % (key, value)
+    ci.setProperty(key, value)
 
-createOrUpdate(ci)
-
-
+create_or_update(ci)
